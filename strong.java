cffb0b06ff03756b872abfa9daca5b58896a5ff8
simhadrikuppala <darlingmr39@gmail.com>
2025-07-23 11:25:39 +0530

    add strong numbers java files via upload

---
+++ strong.java
@@ -0,0 +1,51 @@
+class strong
+{
+public static void main(String[] args)
+	{
+System.out.println("Strong numbers from 1 to 5000 are :");
+for (int i = 1; i <= 50000; i++)
+{
+if (isItStrong(i))
+{			
+System.out.println(i);
+}
+}
+}
+static boolean isItStrong(int num)
+{
+int no = num;
+int sum = 0;
+while (no > 0)
+{
+int digit = no % 10;
+sum += fact(digit);
+ no = no / 10;
+}
+return sum == num;
+}
+static int fact(int digit)
+{
+int f = 1;
+for (int j = digit; j > 1; j--)
+{
+f *= j;
+}
+return f;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+	
