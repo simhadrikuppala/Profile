f0d09fd8a50062dc248d385ca3aa18a8c15ea569
simhadrikuppala <darlingmr39@gmail.com>
2025-07-23 10:48:08 +0530

    add java files via upload

---
+++ prime.java
@@ -0,0 +1,27 @@
+public class prime
+{
+public static void main (String[] args)
+{		
+System.out.println("Prime numbers from 1 to 100 are :");
+for (int i = 2; i <= 100; i++)         
+{ 		  	  
+if(isprime(i))
+{
+ System.out.println(i+"");
+}
+}
+}
+public static boolean isprime(int num)
+{
+if(num<2)
+return false;
+for(int i=2;i<=Math.sqrt(num);i++)
+{
+if(num%i==0)
+return false;
+}
+return true;
+}
+}         
+          
+	
