213344d5ced1fb1f9b9ada38a9843a5aeccd2f69
simhadrikuppala <darlingmr39@gmail.com>
2025-07-23 11:40:52 +0530

    adding prime and strong by using python

---
+++ strong.py
@@ -0,0 +1,15 @@
+import math
+
+maximum = int(input(" Please Enter the Maximum Value: "))
+
+for Number in range(1, maximum):
+    Temp = Number
+    Sum = 0
+    while(Temp > 0):
+        Reminder = Temp % 10
+        Factorial = math.factorial(Reminder)
+        Sum = Sum + Factorial
+        Temp = Temp // 10
+    
+    if (Sum == Number):
+        print(" %d is a Strong Number" %Number)
