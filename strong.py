67310dd4dafdd76b636b62393d3975974e425481
simhadrikuppala <darlingmr39@gmail.com>
2025-08-25 15:32:30 +0530

    Update strong.py

--- strong.py
+++ strong.py
@@ -1,15 +1,21 @@
 import math
 
-maximum = int(input(" Please Enter the Maximum Value: "))
-
-for Number in range(1, maximum):
-    Temp = Number
-    Sum = 0
-    while(Temp > 0):
-        Reminder = Temp % 10
-        Factorial = math.factorial(Reminder)
-        Sum = Sum + Factorial
-        Temp = Temp // 10
+def is_strong_number(num):
+    """
+    Checks if a given number is a strong number.
+    A strong number is one where the sum of the factorials of its digits equals the number itself.
+    """
+    original_num = num
+    sum_of_factorials = 0
     
-    if (Sum == Number):
-        print(" %d is a Strong Number" %Number)
+    while num > 0:
+        digit = num % 10
+        sum_of_factorials += math.factorial(digit)
+        num //= 10
+        
+    return sum_of_factorials == original_num
+
+print("Strong numbers from 1 to 5000:")
+for i in range(1, 5001):
+    if is_strong_number(i):
+        print(i)
